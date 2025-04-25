c2182690dc49d8154d8f9f765f858a1cf4acf19f
Илиевски Даниел <daniel.ilievski.2@students.finki.ukim.mk>
2025-04-25 18:15:48 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/soa/internshipmanagement/InternshipManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.soa.internshipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/soa/internshipmanagement/InternshipManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.soa.internshipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
