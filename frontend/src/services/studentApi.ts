a299c3a00c81d091c950a364baa8786daab9765b
gorazdbiskoski <gorazdbiskoskii@gmail.com>
2025-10-02 17:28:02 +0200

    feat: implement accepting/rejecting internship proposals as student

--- frontend/src/services/studentApi.ts
+++ frontend/src/services/studentApi.ts
@@ -1,5 +1,6 @@
 import {api} from "@/services/api.ts";
 import {CreateInternshipWeekPayload, EditInternshipWeekPayload, InternshipView} from "@/types/internship.ts";
+import {AcceptInternshipCommandDto, RejectInternshipCommandDto} from "@/types/InternshipCommands.ts";
 
 export const studentQueryApi = {
 
@@ -25,6 +26,16 @@ export const studentCommandsApi = {
 
   deleteSearchingInternship: async (): Promise<void> => {
     await api.delete(`/student/submitCommand/DeleteSearchingInternship`);
+  },
+
+  acceptInternship: async(internshipId: string): Promise<void> => {
+    let acceptInternshipCommandDto : AcceptInternshipCommandDto = { internshipId }
+    await api.post(`/student/submitCommand/AcceptInternship`, acceptInternshipCommandDto)
+  },
+
+  rejectInternship: async(internshipId: string): Promise<void> => {
+    let rejectInternshipCommandDto : RejectInternshipCommandDto = { internshipId }
+    await api.post(`/student/submitCommand/RejectInternship`, rejectInternshipCommandDto)
   },
 
   createInternshipWeek: async (payload: CreateInternshipWeekPayload): Promise<void> => {
---
+++ frontend/src/types/InternshipCommands.ts
@@ -0,0 +1,9 @@
+// TypeScript interfaces for the Internship Command DTOs
+
+export interface AcceptInternshipCommandDto {
+    internshipId: string
+}
+
+export interface RejectInternshipCommandDto {
+    internshipId: string
+}
